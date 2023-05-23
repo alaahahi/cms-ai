4710975587da228bdb4fc5caedd65daf3253927c
alaahahi <alaa.hahe@gmail.com>
2023-05-24 00:38:23 +0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title inertia>دائرة الصحة - العقد الإلكتروني</title>
+        <title inertia>الهدف المباشر - العقد الإلكتروني</title>
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
         @routes
         @vite('resources/js/app.js')
