9642f8922df1bf17250fcde13af1def159de501e
alaahahi <alaa.hahe@gmail.com>
2023-11-18 11:01:27 +0300

    Update ModalAddSales.vue

--- resources/js/Components/ModalAddSales.vue
+++ resources/js/Components/ModalAddSales.vue
@@ -121,7 +121,6 @@ const calculateBox = () => {
                         <input
                           id="card"
                           type="number"
-                        
                           class="mt-1 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                           v-model="form.amount" />
                         </div>
@@ -132,7 +131,6 @@ const calculateBox = () => {
                         <input
                           id="card"
                           type="number"
-                          @input="calculateAmount"
 
                           class="mt-1 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                           v-model="form.box"   />
