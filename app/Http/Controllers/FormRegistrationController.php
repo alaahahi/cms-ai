9f2d922fbce9d1a6e67b0c4876477bcf81e77775
alaahahi <alaa.hahe@gmail.com>
2024-03-30 15:04:52 +0300

    Update FormRegistrationController.php

--- app/Http/Controllers/FormRegistrationController.php
+++ app/Http/Controllers/FormRegistrationController.php
@@ -144,7 +144,7 @@ class FormRegistrationController extends Controller
         $to = $_GET['to'] ?? 0;
         $print = $_GET['print'] ?? 0;
         $config = SystemConfig::first();
-        $hospital = Hospital::last();
+        $hospital = Hospital::latest('created_at')->first();
 
 
         $data = Profile::with('user')->orderBy('no', 'DESC');
