ec9ecb228892c6b12b4e0b5d57b1c02766134a31
alaahahi <alaa.hahe@gmail.com>
2025-01-18 02:44:26 +0300

    Update CardsController.php

--- app/Http/Controllers/Api/CardsController.php
+++ app/Http/Controllers/Api/CardsController.php
@@ -25,12 +25,10 @@ class CardsController extends Controller
     {
         // تحديد البطاقات الفعالة فقط باستخدام scope 'Active'
         $cards = Card::active()->get();
-    
-        // إذا تم إرسال card_id في الطلب، استخدمه، إذا لم يكن هناك card_id، استخدم جميع البطاقات الفعالة
-        $cardIds = $request->has('card_id') ? (array) $request->card_id : $cards->pluck('id')->toArray();
+
     
         // الحصول على الخدمات المرتبطة بالبطاقات الفعالة
-        $activeCardServices = CardService::whereIn('card_id', $cardIds)
+        $activeCardServices = CardService::where('card_id', $request->card_id )
             ->where('expir_date', '>=', now())  // فقط الخدمات التي لم تنته صلاحيتها
             ->get();
     
