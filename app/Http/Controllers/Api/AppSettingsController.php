2f3ab2ced752a87573ecd091d8b80066ecaeab2e
alaahahi <alaa.hahe@gmail.com>
2025-01-18 00:04:46 +0300

    Update AppSettingsController.php

--- app/Http/Controllers/Api/AppSettingsController.php
+++ app/Http/Controllers/Api/AppSettingsController.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
-use App\Http\Controllers\Controller\Api;
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
