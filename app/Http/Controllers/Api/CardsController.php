19534d280b672e8dcc052b826901de4499289a04
alaahahi <alaa.hahe@gmail.com>
2025-01-18 02:42:15 +0300

    Update CardsController.php

--- app/Http/Controllers/Api/CardsController.php
+++ app/Http/Controllers/Api/CardsController.php
@@ -23,14 +23,17 @@ class CardsController extends Controller
 
     public function activeCardServices(Request $request)
     {
-        // الحصول على البطاقة الفعالة
-        $cards = Card::active()->get();  // تحديد البطاقات الفعالة فقط باستخدام scope 'Active'
-
+        // تحديد البطاقات الفعالة فقط باستخدام scope 'Active'
+        $cards = Card::active()->get();
+    
+        // إذا تم إرسال card_id في الطلب، استخدمه، إذا لم يكن هناك card_id، استخدم جميع البطاقات الفعالة
+        $cardIds = $request->has('card_id') ? $request->card_id : $cards->pluck('id');
+    
         // الحصول على الخدمات المرتبطة بالبطاقات الفعالة
-        $activeCardServices = CardService::whereIn('card_id', $request->card_id  ? $request->card_id: $cards->pluck('id'))
+        $activeCardServices = CardService::whereIn('card_id', $cardIds)
             ->where('expir_date', '>=', now())  // فقط الخدمات التي لم تنته صلاحيتها
             ->get();
-
+    
         // التحقق من وجود خدمات فعالة
         if ($activeCardServices->isEmpty()) {
             return response()->json([
@@ -38,11 +41,12 @@ class CardsController extends Controller
                 'message' => 'No active card services found.',
             ], 404);
         }
-
+    
         return response()->json([
             'status' => 'success',
             'data' => $activeCardServices,
         ], 200);
     }
+    
     
 }
