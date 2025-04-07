91ca0d7b6fd2a26284546af954f8e0686719733b
alaahahi <alaa.hahe@gmail.com>
2025-04-07 13:16:20 +0300

    Update PendingRequest.vue

--- resources/js/Pages/FormRegistration/PendingRequest.vue
+++ resources/js/Pages/FormRegistration/PendingRequest.vue
@@ -380,12 +380,12 @@ function confirmEditPendingRequest(V) {
                     </td>
                     <td className="border px-4 py-2 td">
                       <a
-                        :href="`/public/storage/${user.image}`"
+                        :href="`${user.image}`"
                         style="cursor: pointer"
                         target="_blank">
                         <img
                         v-if="user.image"
-                        :src="`/public/storage/${user.image}`"
+                        :src="`${user.image}`"
                         alt="Setting Image"
                         style="width:200px;"
                         />
