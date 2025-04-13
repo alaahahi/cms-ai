43b38d57823983fbcd35f7eab2825b097e5e3adf
ALAA HAHI <alaa.hahe@gmail.com>
2025-04-13 18:20:00 +0300

    Update CardsController.php

--- app/Http/Controllers/Api/CardsController.php
+++ app/Http/Controllers/Api/CardsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Card;
 use App\Models\CardService;
+use App\Models\UserType;
 use App\Models\Profile;
 use App\Models\Category;
 use App\Models\PendingRequest;
